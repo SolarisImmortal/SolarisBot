61ac4958ecb9f9d75741353714d5ea18001571b2
SolarisImmortal <ImmortalSolaris@gmail.com>
2016-08-30 20:35:51 -0700

    Added SOLARR.py
    This file will add methods for setting and getting data from google
    spreadsheets in an organized way.

---
+++ SOLARR.py
@@ -0,0 +1,17 @@
+
+# SOLARR is just a filler name until a better one is created.
+
+# This file provides NEARR and FARR with some baseline features
+# as well as features specific to FARR or NEARR.
+# The management of these features is performed in another file.
+
+# Sets the blacklist value for the user
+def set_blacklist(user, value):
+    
+    return True
+
+# Returns True if the user is blacklisted
+def is_blacklisted(users):
+    
+    return False
+
